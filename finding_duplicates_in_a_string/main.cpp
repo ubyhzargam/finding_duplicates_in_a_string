8b3669c78b54de302bfb4aec93f1c219649738f2
ubyhzargam <ubyhzargam@gmail.com>
2023-06-26 12:52:44 +0530

    Update main.cpp

--- finding_duplicates_in_a_string/main.cpp
+++ finding_duplicates_in_a_string/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int l=0,c=0;
+    cout<<"Enter a string to find duplicates in it : "<<endl;
+    char str[50];
+    cin>>str;
+    for(int i=0;str[i]!='\0';i++)l++;
+    for(int i=0;i<l;i++)
+    {
+        for(int j=i+1;j<=l;j++)
+        {
+            if(str[i]==str[j])
+                c++;
+        }
+        if(c>0)
+            cout<<"The character "<< str[i] <<" is repeated "<<c<<" times. "<<endl;
+        c=0;
+    }
     return 0;
 }
